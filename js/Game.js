c7cce07e7a7a65e64d9dac10c756c686c26f071e
Adam Haywood <adambhaywood@gmail.com>
2020-02-13 21:44:32 -0500

    getting started on project 4

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  contructor() {
+    this.missed = 0;
+    this.phrases = [];
+    this.activePhase = null;
+  }
+}
