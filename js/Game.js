d7b47461b821b8d55e21f4858bf2c6bf9173b578
Adam Haywood <adambhaywood@gmail.com>
2020-02-27 03:15:30 -0500

    got through step 6 in project

--- js/Game.js
+++ js/Game.js
@@ -3,9 +3,28 @@
  * Game.js */
 
 class Game {
-  contructor() {
+  constructor() {
     this.missed = 0;
-    this.phrases = [];
     this.activePhase = null;
+    this.phrases = this.createPhrases();
   }
+  
+  createPhrases() {
+    let newPhrase = [];
+    let newPhrase0 = new Phrase('Shiny black coupe at night look like a goblin');
+    let newPhrase1 = new Phrase('All I need is one mic');
+    let newPhrase2 = new Phrase('You shouldnt throw stones if you live in a glass house');
+    let newPhrase3 = new Phrase('But my plan is to show you that I understand');
+    let newPhrase4 = new Phrase('Money power respect what you need in life');
+    newPhrase.push(newPhrase0,newPhrase1,newPhrase2,newPhrase3,newPhrase4);
+    return newPhrase;
+  }
+
+  getRandomPhrase() {
+    let i = Math.floor(Math.random() * 5);
+    return this.phrases[i];
+  }
+
 }
+
+
--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,17 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
+  
+  addPhraseToDisplay() {
+    const phraseList = document.querySelector('#phrase ul');
+    let singleChars = [...this.phrase];
+    singleChars.forEach((char) => {
+      if (char === ' ') {
+        phraseList.append(`<li class="space">${char}</li>`)
+      } else {
+        phraseList.append(`<li class="hide letter ${char}">${char}</li>`)
+      }
+    })
+    
+  }
 }
